c29da651e5867db1cee21c2fc55fbc23cf74807d
whopwhop <sylwia.erhardt@gmail.com>
2014-06-16 02:51:53 +0200

    Slide item based on url

--- js/a.js
+++ js/a.js
@@ -1,47 +1,97 @@
 var AKO = {};
 
-AKO.tabs = function () {
-    $('#tabs').tabs();
-
-    $('#tabs').bind('click', function(e) {
-        window.location.hash = e.target.hash;
-        $('.selected').removeClass('selected');
-        $(e.target).addClass('selected');
-    });
-};
-
-AKO.accordition = function () {
-    $('#life-insurance').accordion(
-        {
-            header: 'a.header',
-            heightStyle: 'content',
-            collapsible: true,
-            active: false,
-            navigation: true,
-            icons: {
-                header: 'icon-down-open',
-                activeHeader: 'icon-up-open'
-            },
-            activate: function (event, ui) {
-                $(ui.oldPanel.context).removeClass('header-open');
-                $(ui.newPanel.context).addClass('header-open');
-
-            }
-        }
-    );
-};
 AKO.toogleAll = function () {
+
     $('.change-panels-state').click(function() {
-        $('.ui-accordion-header:not(.ui-state-active)').toggleClass('header-open').next().slideToggle();
-        $('.title').text($(this).text() == 'Rozwiń wszystkie' ? 'Zwiń wsyzstkie' : 'Rozwiń wszystkie');
+        $('.header').toggleClass('header-closed').next().slideToggle();
+        $('.caption').text($(this).text() == 'Rozwiń wszystkie' ? 'Zwiń wszystkie' : 'Rozwiń wszystkie');
         $(this).toggleClass('show-all');
         return false;
     });
+};
 
+AKO.toggleContent = function () {
+    var containers = $('.content section'),
+        headers = $('.header'),
+        i;
+
+    headers.click(function (e) {
+        for (i = 0; i < containers.length; i++) {
+            if ($(this).hasClass('header-closed')) {
+                containers.slideUp();
+                headers.addClass('header-closed');
+            }
+        }
+        $(e.target).toggleClass('header-closed');
+        $(e.target.hash).slideToggle();
+        window.location.hash = e.target.hash;
+        return false;
+    });
+};
+
+AKO.toggleFromUrl = function () {
+    var url = window.location.hash,
+        containers = ['.section-1', '.section-2'],
+        i,j, el;
+
+    $('document').ready(function() {
+        setTimeout(function() {
+            console.log($('a[href="'+ url +'"]')[0].hash);
+//            if ($('a[href="'+ url +'"]')[0].hash == url) {
+//                $($('a[href="'+ url +'"]')[0]).trigger('click');
+//                console.log('e');
+//            }
+//            else {
+                for (i = 0; i < containers.length; i++) {
+                    el =  $('section', containers[i]);
+                    for (j = 0; j < el.length; j++) {
+                        if ('#' + $(el)[j].id == url) {
+                            $('a[href="#'+ $(containers[i])[0].id + '"]').trigger('click');
+                            $($($(el)[j])[0].previousElementSibling).trigger('click');
+                        }
+                    }
+                }
+//            }
+        },10);
+    });
+};
+
+AKO.switchMenu = function () {
+    var menuItems = $('#tabs li'),
+        menuItemActiveId,
+        containers = ['.section-1', '.section-2'],
+        i;
+
+    menuItems.click(function (e) {
+        $('a', menuItems).removeClass('selected');
+        $('a', $(this)).addClass('selected');
+        menuItemActiveId = e.target.hash;
+
+        for (i = 0; i < containers.length; i++) {
+            $(containers[i]).hide();
+        }
+        $(menuItemActiveId).slideDown();
+    });
+};
+
+AKO.markCheckbox = function () {
+    $('#checkbox-1').click(function () {
+        if ($(this).prop('checked')) {
+            $(this).attr({checked: 'checked'});
+        }
+        else {
+            $(this).removeAttr('checked');
+        }
+    });
 };
 
 AKO.init = function () {
-    AKO.tabs();
-    AKO.accordition();
+    AKO.toggleFromUrl();
     AKO.toogleAll();
+    AKO.switchMenu();
+    AKO.toggleContent();
+    AKO.markCheckbox();
+
 };
+
+
